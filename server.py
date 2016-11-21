352f6a52b8da7a8575c9f05303001368fb380721
Fernando Gutierrez Garcia <fgutierr@gamma34.aulas.gsyc.urjc.es>
2016-11-21 10:12:07 +0100

    Envio correcto de respuestas Trying, Ring y 200 OK al cliente cuando se recibe un INVITE

--- server.py
+++ server.py
@@ -5,27 +5,33 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+import os
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPHandler(socketserver.DatagramRequestHandler):
     """
-    Echo server class
+    SIP server class
     """
-
+    IP = sys.argv[1]
+	PUERTO = sys.argv[2]
+	AUDIO = sys.argv[3]
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+        # Leyendo línea a línea lo que nos envía el cliente
+        line = self.rfile.read()
+        print("El cliente nos manda " + line.decode('utf-8'))
+        Request = line.decode('utf-8')
+        if Request.startswith('INVITE'):
+            self.wfile.write(b"SIP/2.0 100 Trying\r\n")
+            self.wfile.write(b"SIP/2.0 180 Ring\r\n")
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        elif Request.startswith('ACK'):
+            aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + AUDIO
+            print "Vamos a ejecutar", aEjecutar
+            os.system(aEjecutar)
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    serv = socketserver.UDPServer(('', 6001), SIPHandler)
+    print("Listening...")
     serv.serve_forever()
