fdb1b599e78a8f553f510413c3b396019fc29697
Fernando Gutierrez Garcia <fgutierr@alpha06.aulas.gsyc.urjc.es>
2016-11-21 19:45:30 +0100

    Server terminado

--- server.py
+++ server.py
@@ -15,14 +15,12 @@ class SIPHandler(socketserver.DatagramRequestHandler):
     """
     IP = sys.argv[1]
     PUERTO = sys.argv[2]
-    
 
     def handle(self):
         # Leyendo línea a línea lo que nos envía el cliente
         line = self.rfile.read()
         Request = line.decode('utf-8')
         print("El cliente nos manda: " + Request)
-        
 
         if Request.startswith('INVITE'):
             self.wfile.write(b"SIP/2.0 100 Trying\r\n")
@@ -31,13 +29,13 @@ class SIPHandler(socketserver.DatagramRequestHandler):
 
         elif Request.startswith('ACK'):
             AUDIO = sys.argv[3]
-            aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " + AUDIO 
+            aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " + AUDIO
             print("Vamos a ejecutar: ", aEjecutar)
             os.system(aEjecutar)
 
         elif Request.startswith('BYE'):
             print("Despidiendo al cliente...")
-        
+
         else:
             self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
 
@@ -53,4 +51,3 @@ if __name__ == "__main__":
 
     except:
         sys.exit("Usage: python3 server.py IP Port Audio_File")
-
